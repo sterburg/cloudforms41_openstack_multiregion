0f048ca89b34566ab06161b91c41370df1205c66
sterburg <sterburg@redhat.com>
2016-05-31 13:19:46 +0200

    Added OpenStack Multi-Region support

---
+++ app/models/manageiq/providers/openstack/regions.rb
@@ -0,0 +1,28 @@
+module ManageIQ
+  module Providers::Openstack
+    module Regions
+      REGIONS = {
+        "ams5" => {
+          :name        => "ams5",
+          :description => "AMS5",
+        },
+        "ams6" => {
+          :name        => "ams6",
+          :description => "AMS6",
+        },
+      }
+
+      def self.all
+        REGIONS.values
+      end
+
+      def self.names
+        REGIONS.keys
+      end
+
+      def self.find_by_name(name)
+        REGIONS[name]
+      end
+    end
+  end
+end
